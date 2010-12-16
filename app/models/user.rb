2c1c0bd2ecdc7810f92dde0e3ec0250195487c64
holtonma <holtonma@gmail.com>
2010-12-15 19:19:36 -0800

    simple devise auth for now

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,7 @@
 require 'digest/sha1'
 
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
   
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-  
-  validates_presence_of     :login
-  validates_length_of       :login,    :within => 3..40
-  validates_uniqueness_of   :login
-  #validates_format_of       :login,    :with => Authentication.login_regex, :message => Authentication.bad_login_message
-
-  #validates_format_of       :name,     :with => Authentication.name_regex,  :message => Authentication.bad_name_message, :allow_nil => true
-  validates_length_of       :name,     :maximum => 100
-
   validates_presence_of     :email
   validates_length_of       :email,    :within => 6..100 #r@a.wk
   validates_uniqueness_of   :email
